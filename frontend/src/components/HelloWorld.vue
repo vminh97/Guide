2457ffd810a1dc29099de522d4cf42f1c96c248d
Tran van minh <minhcntt29091997@gmail.com>
2022-05-15 23:17:24 +0700

    ui

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,43 +1,22 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-    </h3>
+  <div class="example-basic">
+    <el-time-picker v-model="value1" placeholder="Arbitrary time" />
+    <el-time-picker
+      v-model="value2"
+      arrow-control
+      placeholder="Arbitrary time"
+    />
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
+<script>
+import { ref } from 'vue'
+const value1 = ref()
+const value2 = ref()
+</script>
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+<style>
+.example-basic .el-date-editor {
+  margin: 8px;
 }
 </style>
